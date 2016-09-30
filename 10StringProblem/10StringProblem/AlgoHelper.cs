b9ad6c574d9e5a5bc784d6e5a0cd19c0d9010b73
minal_444 <minal_444@hotmail.com>
2016-09-30 01:07:17 -0700

    basic things working no threading no performence optimization yet

---
+++ 10StringProblem/10StringProblem/AlgoHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10StringProblem
+{
+    public class AlgoHelper
+    {
+        private bool AddMore { get; set; }
+        private bool CurrentSequenceValid { get; set; }
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public AlgoHelper()
+        { 
+            AddMore = false;
+            CurrentSequenceValid = false;   
+        }
+
+        /// <summary>
+        /// Main method to validate if number is digit friendly 
+        /// </summary>
+        /// <param name="iNumber"></param>
+        /// <returns></returns>
+        public bool ValidateFriendlyNess(long iNumber)
+        {
+            int[] arrDigits = iNumber.ToString().Select(o => Convert.ToInt32(o)-48).ToArray();
+            long iNumberToValidate = arrDigits[0];
+            if (arrDigits.Length < 2)//checking double digit only 
+                return false;
+            for (int i = 1; i < arrDigits.Length; i++)
+            {
+
+                iNumberToValidate = Convert.ToInt32(string.Format("{0}{1}", iNumberToValidate , arrDigits[i]));
+                this.CurrentSequenceValid = false;
+
+                if (isNumberTotalFriendly(iNumberToValidate))
+                {
+                    this.CurrentSequenceValid = true;
+                    iNumberToValidate = RemoveFirstDigit(iNumberToValidate.ToString());
+                    continue;
+                }
+                else
+                {
+                    if (this.AddMore)
+                    {
+                        continue;
+                    }
+                    else
+                        return false;
+                }
+            }
+            if (this.CurrentSequenceValid)
+                return true;
+            return false;
+        }
+
+        private int RemoveFirstDigit(string strNumberToValidate)
+        {
+            //find best methoed to remove first digit
+            return Convert.ToInt32(strNumberToValidate.Substring(1, strNumberToValidate.Length - 1));
+        }
+
+        /// <summary>
+        /// compare number with friendly total number from constant
+        /// return property if number can accomodate more number
+        /// </summary>
+        /// <param name="iSubNumber"></param>
+        /// <returns></returns>
+        public bool isNumberTotalFriendly(Int64 iSubNumber)
+        {
+            int result = iSubNumber.ToString().Sum(c => Convert.ToInt32(c)-48);
+            if (result == Constants.FriendlyTotal)
+                return true;
+            else if(result<Constants.FriendlyTotal)
+            {
+                this.AddMore = true;
+                return false;
+            }
+            else
+            {
+                this.AddMore = false;
+                return false;
+            }
+        }
+
+    }
+}
--- 10StringProblem/10StringProblem/Program.cs
+++ 10StringProblem/10StringProblem/Program.cs
@@ -3,13 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.RegularExpressions;
 namespace _10StringProblem
 {
     class Program
     {
+        protected static string inputString; //input variable
+        protected static long inputValue; //input variable after converting to Int64
+        protected static List<long> outputValues = new List<long>(); //output values for print
+        /// <summary>
+        /// Console Main
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+            try
+            {
+                if (ValidateInput())
+                {
+                    ProcessRequest();
+                }
+                PrintOutput();
+                Console.WriteLine(Environment.NewLine + "Press enter to close application");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                //Main exception to catch
+                
+#if (DEBUG)
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();                
+#else 
+                Console.WriteLine("Error occured - Please contact admin" + Environment.NewLine + "Press enter to close application");
+                Console.ReadLine();
+#endif
+
+            }
+            
+            
+        }
+
+        /// <summary>
+        /// print output to console
+        /// </summary>
+        private static void PrintOutput()
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            for (int i = 0; i < outputValues.Count; i++)
+            {
+                sb.Append(outputValues[i].ToString());
+                sb.Append(", ");
+
+            }
+            sb.Append("]");
+            Console.WriteLine(sb.ToString());
+        }
+
+        /// <summary>
+        /// Process request if input is valid
+        /// </summary>
+        private static void ProcessRequest()
+        {
+            try
+            {
+                
+                inputValue = Convert.ToInt64(inputString);
+                for (long i = 0; i < inputValue; i++)
+                {
+                    AlgoHelper helper = new AlgoHelper();
+                    if (helper.ValidateFriendlyNess(i))
+                        outputValues.Add(i);
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// validate user input
+        /// </summary>
+        /// <returns></returns>
+        private static bool ValidateInput()
+        {
+            try
+            {
+                Console.WriteLine("Please enter the input string");
+                inputString = Console.ReadLine();
+                Match m = Regex.Match(inputString, @"^[0-9]+$");
+                return m.Success;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
         }
     }
 }
